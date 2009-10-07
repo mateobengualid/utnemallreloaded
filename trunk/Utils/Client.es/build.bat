196ab6002f4028a4203e5d96fb3dfbf9e4a23fd9
alexis.ferreyra <alexis.ferreyra@92e9a0c8-a896-11de-a86f-b10fb6d3a35f>
2009-10-07 02:10:28 +0000

    Build files for DataModel and DataModelClient added.
    USE DATAMODEL files on CLIENT.ES :)

---
+++ trunk/Utils/Client.es/build.bat
@@ -0,0 +1,5 @@
+cd ..\..\Main\Server\WPFCore\compiler\
+metadppc.exe ..\..\..\..\Utils\Client.es\DataModel.dpp
+zoec.exe -ro ..\..\..\..\Utils\Client.es\DataModel.zoe -opath:..\..\..\..\Utils\Client.es\output -ra:"pretty"
+
+pause
---
+++ trunk/Utils/Client.es/buildclient.bat
@@ -0,0 +1,5 @@
+cd ..\..\Main\Server\WPFCore\compiler\
+metadppc.exe ..\..\..\..\Utils\Client.es\DataModelClient.dpp
+zoec.exe -ro ..\..\..\..\Utils\Client.es\DataModelClient.zoe -opath:..\..\..\..\Utils\Client.es\outputclient -ra:"pretty"
+
+pause
