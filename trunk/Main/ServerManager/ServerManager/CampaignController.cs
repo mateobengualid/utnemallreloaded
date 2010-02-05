a02e9b5086c7a7b940417cb8253453f5a5e1520a
apoteoticos <apoteoticos@92e9a0c8-a896-11de-a86f-b10fb6d3a35f>
2010-02-05 23:59:51 +0000

    Parte de campañas

---
+++ trunk/Main/ServerManager/ServerManager/CampaignController.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using UtnEmall.ServerManager;
+using UtnEmall.Server.EntityModel;
+using System.Reflection;
+using System.ServiceModel;
+
+namespace UtnEmall.ServerManager
+{
+    class CampaignController : ControllerBase
+    {
+		private EditServices editServices;
+
+        /// <summary>
+        /// La entidad seleccionada por el administrador.
+        /// </summary>
+        public override IEntity Selected
+        {
+            get
+            {
+                return ((CampaignManager)Manager).ItemList.Selected;
+            }
+        }
+
+        /// <summary>
+        /// El modo del editor puede ser Agregar o Editar
+        /// </summary>
+        public override EditionMode Mode
+        {
+            get
+            {
+                return ((CampaignEditor)Editor).Mode;
+            }
+
+            set
+            {
+                ((CampaignEditor)Editor).Mode = value;
+            }
+        }
+
+        /// <summary>
+        /// La entidad que está siendo agregada o modificada en el editor.
+        /// </summary>
+        public override IEntity Entity
+        {
+            get
+            {
+                return (IEntity)((CampaignEditor)Editor).Campaign;
+            }
+
+            set
+            {
+                ((CampaignEditor)Editor).Campaign = (CampaignEntity)value;
+            }
+        }
+
+        /// <summary>
+        /// Constructor de clase
+        /// </summary>
+        /// <param name="control">Una referencia al control que contiene a este componente</param>
+        /// <param name="manager">El componente que muestra un listado de entidades</param>
+        /// <param name="editor">El componente que permite agregar o editar una entidad</param>
+        /// <param name="firstElement">El componente que debe ser seleccionado cuando se muestra el editor</param>
+        public CampaignController(UserControl1 control, CampaignManager manager, CampaignEditor editor,
+            FrameworkElement firstElement, EditServices editServices)
+            : base(control, manager, editor, firstElement, new LoadList(CampaignManager.Load),
+            new SaveEntity(CampaignManager.Save),
+            new RemoveEntity(CampaignManager.Delete))
+        {
+            CampaignManager campaignManager = (CampaignManager)manager;
+            CampaignEditor addCampaign = (CampaignEditor)editor;
+
+            this.editServices = editServices;
+
+            addCampaign.OkSelected += OnOkSelected;
+            addCampaign.CancelSelected += OnCancelSelected;
+
+            campaignManager.ItemList.NewButtonSelected += OnNewSelected;
+            campaignManager.ItemList.EditButtonSelected += OnEditSelected;
+            campaignManager.ItemList.DeleteButtonSelected += OnDeleteSelected;
+            campaignManager.ItemList.ExtraButtonSelected += OnExtraSelected;
+
+            editServices.OkSelected += new EventHandler(OnEditServicesOkSelected);
+            editServices.CancelSelected += new EventHandler(OnEditServicesCancelSelected);
+        }
+
+        /// <summary>
+        /// Método invocado cuando se presiona el botón OK
+        /// </summary>
+        /// <param name="sender">El objeto que genera al evento</param>
+        /// <param name="e">Contiene infomación adicional acerca del evento</param>
+        void OnEditServicesOkSelected(object sender, EventArgs e)
+        {
+            Control.HideLastShown();
+            Manager.Visibility = Visibility.Visible;
+            Control.LastElementShown = Manager;
+
+            Save(editServices.Campaign);
+        }
+
+        /// <summary>
+        /// Método invocado cuando se presiona el botón Cancelar
+        /// </summary>
+        /// <param name="sender">El objeto que genera al evento</param>
+        /// <param name="e">Contiene infomación adicional acerca del evento</param>
+        void OnEditServicesCancelSelected(object sender, EventArgs e)
+        {
+            Control.HideLastShown();
+            Manager.Visibility = Visibility.Visible;
+            Control.LastElementShown = Manager;
+        }
+
+        /// <summary>
+        /// Borrar el elemento seleccionado
+        /// </summary>
+        /// <returns>la entidad eliminada</returns>
+        protected override IEntity DeleteSelected()
+        {
+            return ((CampaignManager)Manager).ItemList.DeleteSelected();
+        }
+
+        /// <summary>
+        /// Recargar el contenido del administrador
+        /// </summary>
+        /// <param name="args">El argumento del hilo de carga</param>
+        protected override void Reload(LoaderArguments args)
+        {
+            CampaignManager campaignManager = ((CampaignManager)Manager);
+            campaignManager.Clear();
+
+            foreach (IEntity entity in args.Items)
+            {
+                campaignManager.list.Add(entity);
+            }
+        }
+
+        /// <summary>
+        /// Método invocado cuando se presiona el botón Nuevo
+        /// </summary>
+        /// <param name="sender">El objeto que genera al evento</param>
+        /// <param name="e">Contiene infomación adicional acerca del evento</param>
+        protected override void OnNewSelected(object sender, EventArgs e)
+        {
+            Mode = EditionMode.Add;
+            Control.HideLastShown();
+            Editor.Visibility = Visibility.Visible;
+            Control.LastElementShown = Editor;
+            FocusFirst();
+        }
+
+        /// <summary>
+        /// Método invocado cuando se presiona el botón Editar
+        /// </summary>
+        /// <param name="sender">El objeto que genera al evento</param>
+        /// <param name="e">Contiene infomación adicional acerca del evento</param>
+        protected override void OnEditSelected(object sender, EventArgs e)
+        {
+            CampaignEditor addCampaign = (CampaignEditor)Editor;
+            CampaignEntity campaign;
+            Mode = EditionMode.Edit;
+
+            if ((campaign = (CampaignEntity)Selected) == null)
+            {
+                Util.ShowErrorDialog(UtnEmall.ServerManager.Properties.Resources.NoCampaignSelected);
+                return;
+            }
+
+            addCampaign.Campaign = campaign;
+
+            if (campaign == null)
+            {
+                Util.ShowErrorDialog(UtnEmall.ServerManager.Properties.Resources.NoCampaignSelected);
+                return;
+            }
+
+            Control.HideLastShown();
+            addCampaign.Visibility = Visibility.Visible;
+            Control.LastElementShown = addCampaign;
+        }
+
+        /// <summary>
+        /// Método invocado cuando el botón extra es seleccionado en el administrador
+        /// </summary>
+        /// <param name="sender">El objeto que envía la señal</param>
+        /// <param name="e">Los argumentos del evento</param>
+        protected override void OnExtraSelected(object sender, EventArgs e)
+        {
+            CampaignEntity entity;
+
+            if ((entity = (CampaignEntity)Selected) == null)
+            {
+                Util.ShowErrorDialog(UtnEmall.ServerManager.Properties.Resources.NoCampaignSelected);
+                return;
+            }
+
+            editServices.Clear();
+
+            try
+            {
+                foreach (ServiceEntity service in Services.Service.GetAllService(false, Control.Session))
+                {
+                    editServices.AddSourceService(service);
+                }
+            }
+            catch (TargetInvocationException)
+            {
+                Util.ShowErrorDialog(UtnEmall.ServerManager.Properties.Resources.ConnectionErrorServicesNotLoaded);
+                return;
+            }
+            catch (CommunicationException)
+            {
+                Util.ShowErrorDialog(UtnEmall.ServerManager.Properties.Resources.ConnectionErrorServicesNotLoaded);
+                return;
+            }
+
+            foreach (ServiceCampaignEntity serviceCampaign in entity.ServiceCampaign)
+            {
+                editServices.AddDestinationService(serviceCampaign.Service);
+            }
+
+            editServices.Campaign = entity;
+            Control.HideLastShown();
+            editServices.Visibility = Visibility.Visible;
+            Control.LastElementShown = editServices;
+        }
+    }
+}
---
+++ trunk/Main/ServerManager/ServerManager/CampaignManager.xaml.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Navigation;
+using UtnEmall.Server.EntityModel;
+using System.Reflection;
+using System.ServiceModel;
+using System.Collections.ObjectModel;
+using System.Collections.Generic;
+
+namespace UtnEmall.ServerManager
+{
+    /// <summary>
+    /// Esta clase define el componente visual para administrar campañas
+    /// </summary>
+    public partial class CampaignManager
+    {
+        #region Instance Variables and Properties
+
+        /// <summary>
+        /// Un listado de componentes que contienen todos los elementos a mostrar
+        /// </summary>
+        public ItemList ItemList
+        {
+            get { return list; }
+        }
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Constructor de clase
+        /// </summary>
+        public CampaignManager()
+        {
+            this.InitializeComponent();
+            System.Collections.Generic.List<DataBinding> titles = new System.Collections.Generic.List<DataBinding>();
+            titles.Add(new DataBinding(UtnEmall.ServerManager.Properties.Resources.Name, "Name"));
+            titles.Add(new DataBinding(UtnEmall.ServerManager.Properties.Resources.Description, "Description"));
+            list.SetColumns(new Collection<DataBinding>(titles));
+
+            list.DoFilter = DoFilter;
+            list.ExtraButton.Visibility = Visibility.Visible;
+            list.ExtraButton.Content = UtnEmall.ServerManager.Properties.Resources.Services;
+        }
+
+        #endregion
+
+        #region Instance Methods
+
+        #region Public Instance Methods
+
+        /// <summary>
+        /// Elimina los grupos
+        /// </summary>
+        public void Clear()
+        {
+            list.Clear();
+        }
+
+        /// <summary>
+        /// Método para cargar las entidades desde un servicio web
+        /// </summary>
+        /// <param name="loadRelation">indica si se debe cargar las relaciones de cada entidad</param>
+        /// <param name="session">la clave de sesión</param>
+        /// <returns>un listado de entidades</returns>
+        public static ReadOnlyCollection<IEntity> Load(bool loadRelation, string session)
+        {
+            List<IEntity> list = new List<IEntity>();
+
+            foreach (IEntity entity in Services.Campaign.GetAllCampaign(true, session))
+            {
+                list.Add(entity);
+            }
+
+            return (new ReadOnlyCollection<IEntity>(list));
+        }
+
+        /// <summary>
+        /// Guarda la entidad en el servidor
+        /// </summary>
+        /// <param name="campaign">La entidad a ser guardada</param>
+        /// <param name="session">La clave de sesión</param>
+        /// <returns>null si se guardó exitósamente, una entidad con errores en el atributo Errors si falló</returns>
+        public static IEntity Save(IEntity campaign, string session)
+        {
+            return Services.Campaign.Save((CampaignEntity)campaign, session);
+        }
+
+        /// <summary>
+        /// Elimina una entidad del servidor
+        /// </summary>
+        /// <param name="group">La entidad a ser eliminada</param>
+        /// <param name="session">La clave de sesión</param>
+        /// <returns>null si se eliminó con éxtio, una entidad con errores en el atributo Errors si falló</returns>
+        public static IEntity Delete(IEntity campaign, string session)
+        {
+            return Services.Campaign.Delete((CampaignEntity)campaign, session);
+        }
+
+        #endregion
+
+        #region Protected and Private Instance Methods
+
+        /// <summary>
+        /// corre el filtro en obj
+        /// </summary>
+        /// <param name="obj">
+        /// el objeto al cual se le aplicará el filtro
+        /// </param>
+        /// <param name="filterText">
+        /// el texto que filtra la lista
+        /// </param>
+        /// <returns>
+        /// true si oj pasa el filtro
+        /// </returns>
+        private bool DoFilter(Object obj, string filterText)
+        {
+            if (string.IsNullOrEmpty(filterText))
+            {
+                return true;
+            }
+
+            CampaignEntity campaign = (CampaignEntity)obj;
+
+            if (campaign.Name.Contains(filterText)
+                || campaign.Description.Contains(filterText))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        #endregion
+
+        #endregion
+    }
+}
