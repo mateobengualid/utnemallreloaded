7751598dd65039a422d5321e5847b2cc76016a00
alexis.ferreyra <alexis.ferreyra@92e9a0c8-a896-11de-a86f-b10fb6d3a35f>
2010-02-23 23:47:31 +0000

    agregado de cosas jua

---
+++ Main/Server/SampleDatabaseFiller/AccesoriesFiller.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UtnEmall.Server.Base;
+using UtnEmall.Server.DataModel;
+using System.Data;
+using UtnEmall.Store919.EntityModel;
+using UtnEmall.Store919;
+//using UtnEmall.Store855.EntityModel;
+//using UtnEmall.Store855;
+//using UtnEmall.Store930.EntityModel;
+//using UtnEmall.Store930;
+
+namespace SampleDatabaseFiller
+{
+    class AccesoriesFiller
+    {
+        IDbConnection conn;
+
+        public AccesoriesFiller(IDbConnection connection)
+        {
+            this.conn = connection;
+        }
+
+        public void Run()
+        {
+            FillCategorias();
+            FillAccesorios();
+        }
+
+        private void FillAccesorios()
+        {
+        }
+
+        private void FillCategorias()
+        {
+        }
+    }
+}
---
+++ Main/Server/SampleDatabaseFiller/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UtnEmall.Server.Base;
+using UtnEmall.Server.DataModel;
+using System.Data;
+
+namespace SampleDatabaseFiller
+{
+    class Program
+    {
+        static IDbConnection conn;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Abriendo conexion a base de datos");
+
+            conn = DataAccessConnection.Instance.GetNewConnection();
+
+            FillRegistersForLibrary();
+            FillRegistersForCinema();
+            FillRegistersForAccesories();
+            FillRegistersForMall();
+
+            Console.WriteLine("Fin");
+            Console.Read();
+        }
+
+        private static void FillRegistersForMall()
+        {
+            Console.WriteLine("Llenando registros para Mall");
+        }
+
+        private static void FillRegistersForAccesories()
+        {
+            Console.WriteLine("Llenando registros para Accesorios");
+            new AccesoriesFiller(conn).Run();
+        }
+
+        private static void FillRegistersForCinema()
+        {
+            Console.WriteLine("Llenando registros para Cine");
+        }
+
+        private static void FillRegistersForLibrary()
+        {
+            Console.WriteLine("Llenando registros para Libreria");
+        }
+
+
+    }
+}
